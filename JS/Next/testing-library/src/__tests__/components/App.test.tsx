aba729b77ae135f20df1d5bad37d7406f38249bf
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-01-31 10:59:11 +0900

    feat: use userEvent

--- JS/Next/testing-library/src/__tests__/components/App.test.tsx
+++ JS/Next/testing-library/src/__tests__/components/App.test.tsx
@@ -1,11 +1,18 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import App from '@/components/App'
 
 describe('App', () => {
-  test('renders App component', () => {
+  test('renders App component', async () => {
     render(<App />)
 
-    // screen.debug();
-    expect(screen.getByText('Hello React')).toBeInTheDocument()
+    // wait for the user to resolve
+    await screen.findByText(/Signed in as/)
+
+    expect(screen.queryByText(/Searches for JavaScript/)).toBeNull()
+
+    await userEvent.type(screen.getByRole('textbox'), 'JavaScript')
+
+    expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument()
   })
 })
