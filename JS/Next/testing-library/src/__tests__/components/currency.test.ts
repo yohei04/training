495c3366d60fb9a576ff45160606ebd8f5a2cb92
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-02-12 01:24:17 +0900

    Auto Mocking Fetch

--- JS/Next/testing-library/src/__tests__/components/currency.test.ts
+++ JS/Next/testing-library/src/__tests__/components/currency.test.ts
@@ -1,27 +1,46 @@
 import { convert } from '../../utils/currency'
 
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () => Promise.resolve({ rates: { CAD: 1.42 } }),
-  })
-)
+// global.fetch = jest.fn(() =>
+//   Promise.resolve({
+//     json: () => Promise.resolve({ rates: { CAD: 1.42 } }),
+//   })
+// )
 
 beforeEach(() => {
   fetch.mockClear()
 })
 
-test('finds exchange', async () => {
+// test('finds exchange', async () => {
+//   const rate = await convert('USD', 'CAD')
+
+//   expect(rate).toEqual(1.42)
+//   expect(fetch).toHaveBeenCalledTimes(1)
+// })
+
+// test('returns null when exception', async () => {
+//   fetch.mockImplementationOnce(() => Promise.reject('API is down'))
+
+//   const rate = await convert('USD', 'CAD')
+
+//   expect(rate).toEqual(null)
+//   expect(fetch).toHaveBeenCalledWith('https://api.exchangeratesapi.io/latest?base=USD')
+// })
+
+test('finds exchange with jest-fetch-mock', async () => {
+  fetch.mockResponseOnce(JSON.stringify({ rates: { CAD: 1.42 } }))
+
   const rate = await convert('USD', 'CAD')
 
   expect(rate).toEqual(1.42)
   expect(fetch).toHaveBeenCalledTimes(1)
 })
 
-test('returns null when exception', async () => {
-  fetch.mockImplementationOnce(() => Promise.reject('API is down'))
+test('returns null when exception with jest-fetch-mock', async () => {
+  fetch.mockReject(() => Promise.reject('API id down'))
 
   const rate = await convert('USD', 'CAD')
 
   expect(rate).toEqual(null)
+  expect(fetch).toHaveBeenCalledTimes(1)
   expect(fetch).toHaveBeenCalledWith('https://api.exchangeratesapi.io/latest?base=USD')
 })
