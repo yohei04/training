43f150bc2f14c902876dc71de0ccbe6563312e05
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-02-12 01:09:24 +0900

    manual mock

--- JS/Next/testing-library/src/__tests__/components/currency.test.ts
+++ JS/Next/testing-library/src/__tests__/components/currency.test.ts
@@ -1,5 +1,4 @@
 import { convert } from '../../utils/currency'
-jest.mock('../../utils/currency')
 
 global.fetch = jest.fn(() =>
   Promise.resolve({
@@ -7,14 +6,18 @@ global.fetch = jest.fn(() =>
   })
 )
 
-it('concerts USD to CAD', async () => {
+beforeEach(() => {
+  fetch.mockClear()
+})
+
+test('finds exchange', async () => {
   const rate = await convert('USD', 'CAD')
 
   expect(rate).toEqual(1.42)
   expect(fetch).toHaveBeenCalledTimes(1)
 })
 
-it('returns null when exception', async () => {
+test('returns null when exception', async () => {
   fetch.mockImplementationOnce(() => Promise.reject('API is down'))
 
   const rate = await convert('USD', 'CAD')
