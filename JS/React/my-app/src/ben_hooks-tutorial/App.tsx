30b9b82f03167a972886f3eea1d496b4296070c5
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-02-21 19:54:57 +0900

    first setting

---
+++ JS/React/my-app/src/ben_hooks-tutorial/App.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Hello from './Hello';
+
+interface AppProps {}
+
+const App: React.FC<AppProps> = ({}) => {
+  return (
+    <>
+      <Hello />
+    </>
+  );
+};
+
+export default App;
---
+++ JS/React/my-app/src/ben_hooks-tutorial/Hello.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface HelloProps {}
+
+const Hello: React.FC<HelloProps> = ({}) => {
+  return <>hello</>;
+};
+
+export default Hello;
