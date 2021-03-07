f7d29ae326d926278fe1acf943c8aac1c5c297af
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-03-07 11:48:47 +0900

    display todolist

--- JS/React/tailwindreact-app/src/components/TodoList.tsx
+++ JS/React/tailwindreact-app/src/components/TodoList.tsx
@@ -1,19 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface TodoListProps {}
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 const TodoList: React.FC<TodoListProps> = ({}) => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
   useEffect(() => {
     const fetchTodos = async () => {
-      fetch('https://jsonplaceholder.typicode.com/todos')
-        .then((response) => response.json())
-        .then((json) => console.log(json));
+      const fetchData = await fetch(
+        'https://jsonplaceholder.typicode.com/todos'
+      );
+      const data: Todo[] = await fetchData.json();
+      const formatData = data.map((d) => ({
+        id: d.id,
+        title: d.title,
+        completed: d.completed,
+      }));
+      setTodos(formatData);
+      //   .then((response) => response.json())
+      //   .then(json => setTodos(json))
+      // // .then((json) => console.log(json));
     };
 
     fetchTodos();
   }, []);
 
-  return <></>;
+  console.log(todos);
+  return (
+    <>
+      <div className="todoList">
+        {todos.map((t) => (
+          <p>
+            <span className="mr-2">{t.id}</span>
+            <span>{t.title}</span>
+          </p>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default TodoList;
