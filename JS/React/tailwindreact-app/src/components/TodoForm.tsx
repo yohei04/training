76094688aa59f9b21e6a0af455864bc0c0ba4fc2
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-03-13 16:33:07 +0900

    feat: create TodoForm

---
+++ JS/React/tailwindreact-app/src/components/TodoForm.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface TodoFormProps {
+  
+}
+
+const TodoForm: React.FC<TodoFormProps> = ({}) => {
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default TodoForm
