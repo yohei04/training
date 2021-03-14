4e86af756dff16a753cf015e5849504be016f84e
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-03-14 10:24:06 +0900

    refactor: delete hoges

--- JS/React/tailwindreact-app/src/App.tsx
+++ JS/React/tailwindreact-app/src/App.tsx
@@ -1,33 +1,6 @@
 import TodoList from './components/TodoList';
 
 function App() {
-  // export default function Footer({ children, ...restProps }) {
-  //   return <div>{children}</div>;
-  // }
-
-  // Footer.Row = function FooterRow({ children, ...restProps }) {
-  //   return <div {...restProps}>{children}</div>;
-  // };
-
-  // const hoge = (a: number, b: number) => {
-  //   return a + b;
-  // };
-
-  const hoge: any = []
-
-  // const hoge: any = 1
-
-  // let hoge = {}
-  // hoge = "a"
-
-  hoge.fuga = 'fuga';
-  hoge[0] = "aaa"
-
-  hoge.bura = 'bura';
-
-  hoge.func = () => 'return func' as const;
-
-  console.log(hoge[1]);
 
   return (
     <>
