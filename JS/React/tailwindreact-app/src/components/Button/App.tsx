52a9df9a673a32fd215a10ae46989348add481a3
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-03-19 12:12:04 +0900

    feat: first commit for Button

---
+++ JS/React/tailwindreact-app/src/components/Button/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Button } from '.';
+
+
+interface AppProps {
+  
+}
+
+const App: React.FC<AppProps> = ({}) => {
+  return (
+    <>
+      <Button />
+    </>
+  );
+}
+
+export default App
---
+++ JS/React/tailwindreact-app/src/components/Button/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface ButtonProps {
+  
+}
+
+const Button: React.FC<ButtonProps> = ({}) => {
+  return (
+    <>
+      <button>ボタンです</button>
+    </>
+  );
+}
+
+export default Button
