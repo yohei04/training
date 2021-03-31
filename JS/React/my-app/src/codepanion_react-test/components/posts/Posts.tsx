c919ad7f625f96bd44e68a954156ee8bd179c2dd
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-03-31 17:08:41 +0900

    feat: fetch posts

--- JS/React/my-app/src/codepanion_react-test/components/posts/Posts.tsx
+++ JS/React/my-app/src/codepanion_react-test/components/posts/Posts.tsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface PostsProps {}
 
-const Posts: React.FC<PostsProps> = ({}) => {
+interface IPost {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+const Posts: React.FC<PostsProps> = () => {
+  const [posts, setPosts] = useState<IPost[]>([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  }, []);
+
   return (
     <>
       <h1>Posts</h1>
       <ul>
-        <li></li>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
       </ul>
     </>
   );
