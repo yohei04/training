401de7bf055b1c797e0be8db9fba3d01f693ffd4
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-04-03 03:25:55 +0900

    test: add snapshot

--- JS/React/my-app/src/codepanion_react-test/components/posts/AddNewPostBtn.test.tsx
+++ JS/React/my-app/src/codepanion_react-test/components/posts/AddNewPostBtn.test.tsx
@@ -2,6 +2,12 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import AddNewPostBtn from './AddNewPostBtn';
 
 describe('AddNewPostBtn', () => {
+  test('<AddNewPostBtn /> matches snapshot', () => {
+    const mockOnClick = jest.fn();
+    const { asFragment } = render(<AddNewPostBtn onClick={mockOnClick} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+
   test('click button should trigger onClick callback', () => {
     const mockOnClick = jest.fn();
     render(<AddNewPostBtn onClick={mockOnClick} />);
