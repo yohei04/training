c2cf0f25ca49d9837faf30dae15232e37b5645cd
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-04-23 14:19:35 +0900

    feat: create contact page

---
+++ JS/Next/with-typescript-app/pages/contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/Layout';
+
+const contact = () => {
+  return (            
+    <Layout title="contact | Next.js + TypeScript Example" >
+      <h2>これはcontact page です</h2>
+    </Layout>
+  );
+}
+
+export default contact
