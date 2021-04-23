82da16d812c657d620fa9f169cf112b885d6a261
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-04-23 23:34:58 +0900

    feat: create Card componennt

--- JS/Next/with-typescript-app/components/Layout.tsx
+++ JS/Next/with-typescript-app/components/Layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react'
 import Head from 'next/head'
 import Header from './organisms/Header/Header'
+import { Card } from './organisms/Card'
 
 
 type Props = {
@@ -16,6 +17,7 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <Header />
+    <Card />
     {children}
     <footer>
       <hr />
---
+++ JS/Next/with-typescript-app/components/organisms/Card/Card.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button } from "../../atoms/Button";
+
+interface CardProps {}
+
+const Card = (props) => {
+  return (
+    <>
+      <Button color="blue" size="l">
+        ほげ
+      </Button>
+    </>
+  );
+};
+
+export default Card;
