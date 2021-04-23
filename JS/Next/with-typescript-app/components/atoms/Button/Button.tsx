8bba0cc7baa7b57a1ea57c0311d6c64f9f117fae
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-04-23 23:34:28 +0900

    feat: create Button with css modules

--- JS/Next/with-typescript-app/components/atoms/Button/Button.tsx
+++ JS/Next/with-typescript-app/components/atoms/Button/Button.tsx
@@ -1,32 +1,23 @@
+import clsx from "clsx";
 import React from "react";
+
+import styles from "./Button.module.scss";
 
 interface ButtonProps extends React.ComponentPropsWithoutRef<"button"> {
   color: "blue" | "red";
   size: "s" | "m" | "l";
 }
 
-const TWButton = ({ color, size, ...rest }: ButtonProps) => {
-  const buttonStyle = `${baseStyle} ${colorStyle[color]} ${sizeStyle[size]}`;
+const Button = ({ color, size, ...rest }: ButtonProps) => {
   return (
     <>
-      <button className={buttonStyle} {...rest} />
+      <h1 className={styles.text_blue_10}>aaaaaaaaaaa</h1>
+      <button
+        className={clsx(styles.default, styles[color], styles[size])}
+        {...rest}
+      />
     </>
   );
 };
 
-type Valueof<T> = T[keyof T];
-
-const baseStyle = "px-4 py-2 rounded-lg";
-
-const colorStyle: Record<Valueof<Pick<ButtonProps, "color">>, string> = {
-  blue: "bg-blue-400",
-  red: "bg-red-400",
-};
-
-const sizeStyle: Record<Valueof<Pick<ButtonProps, "size">>, string> = {
-  s: "w-32",
-  m: "w-96",
-  l: "w-full",
-};
-
-export default TWButton;
+export default Button;
