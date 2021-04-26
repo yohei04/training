86f47b5f4e14f43a5151dd6e8272e4bb9b9f8d19
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-04-26 16:36:17 +0900

    feat: add space-x-

--- JS/Next/with-typescript-app/components/organisms/Header/Header.tsx
+++ JS/Next/with-typescript-app/components/organisms/Header/Header.tsx
@@ -3,28 +3,26 @@ import Link from "next/link";
 import { Logo } from "../../atoms/Logo";
 
 import styles from "./Header.module.scss";
+import clsx from "clsx";
 
 const Header = () => {
   return (
     <header className={styles.container}>
       <Logo width={40} height={40} />
-      <nav>
+      <nav className={clsx(styles.nav, 'sspace-x-3')}>
         <Link href="/">
           <a>Home</a>
-        </Link>{" "}
-        |{" "}
+        </Link>
         <Link href="/about">
           <a>About</a>
-        </Link>{" "}
-        |{" "}
+        </Link>
         <Link href="/contact">
           <a>Contact</a>
-        </Link>{" "}
-        |{" "}
+        </Link>
         <Link href="/users">
           <a>Users List</a>
-        </Link>{" "}
-        | <a href="/api/users">Users API</a>
+        </Link>
+        <a href="/api/users">Users API</a>
       </nav>
     </header>
   );
