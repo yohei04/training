9c592ac2c7881baa7890a6814d401a49ec1008f3
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-05-09 23:52:52 +0900

    feat: create Post.module.scss

--- JS/Next/with-typescript-app/components/organisms/Post/PostList.tsx
+++ JS/Next/with-typescript-app/components/organisms/Post/PostList.tsx
@@ -2,13 +2,16 @@ import Link from 'next/link';
 import React from 'react';
 import { PostProps } from '../../../pages/posts';
 
+import styles from './Post.module.scss';
+import utilStyles from '../../../styles/utils.module.scss';
+
 const PostList = ({ posts }: PostProps) => {
   return (
     <>
       {posts.map((post) => (
         <Link href="/posts/[id]" as={`/posts/${post.id}`}>
           <a>
-            <div className="post">
+            <div className={styles.container}>
               <h1>{post.title}</h1>
               <p>{post.body}</p>
             </div>
@@ -18,5 +21,4 @@ const PostList = ({ posts }: PostProps) => {
     </>
   );
 };
-
 export default PostList;
