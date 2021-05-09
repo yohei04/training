33be3f4dccef4ae87a94214e148a9edb0a6dc011
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-05-09 23:43:31 +0900

    feat: create Post page

--- JS/Next/with-typescript-app/components/organisms/Post/PostList.tsx
+++ JS/Next/with-typescript-app/components/organisms/Post/PostList.tsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 import { PostProps } from '../../../pages/posts';
 
 const PostList = ({ posts }: PostProps) => {
   return (
     <>
       {posts.map((post) => (
-        <div className="post">
-          <h1>{post.title}</h1>
-          <p>{post.body}</p>
-        </div>
+        <Link href="/posts/[id]" as={`/posts/${post.id}`}>
+          <a>
+            <div className="post">
+              <h1>{post.title}</h1>
+              <p>{post.body}</p>
+            </div>
+          </a>
+        </Link>
       ))}
     </>
   );
 };
 
-export default PostList
+export default PostList;
---
+++ JS/Next/with-typescript-app/pages/posts/[id].tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// interface [id]Props {
+  
+// }
+
+const Post = () => {
+  return (
+    <>
+      <p>aaaa</p>
+    </>
+  );
+}
+
+export default Post;
