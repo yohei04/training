60388a9ff3c32223a1b5705bef1729b33cb5bd76
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-05-10 00:01:00 +0900

    refactor: change some style

--- JS/Next/with-typescript-app/components/organisms/Post/PostList.tsx
+++ JS/Next/with-typescript-app/components/organisms/Post/PostList.tsx
@@ -4,21 +4,22 @@ import { PostProps } from '../../../pages/posts';
 
 import styles from './Post.module.scss';
 import utilStyles from '../../../styles/utils.module.scss';
+import clsx from 'clsx';
 
 const PostList = ({ posts }: PostProps) => {
   return (
-    <>
+    <div className={styles.container}>
       {posts.map((post) => (
         <Link href="/posts/[id]" as={`/posts/${post.id}`}>
           <a>
-            <div className={styles.container}>
-              <h1>{post.title}</h1>
-              <p>{post.body}</p>
+            <div className={clsx(styles.item, utilStyles.m_10)}>
+              <h4>タイトル：{post.title}</h4>
+              <p>本文：{post.body}</p>
             </div>
           </a>
         </Link>
       ))}
-    </>
+    </div>
   );
 };
 export default PostList;
