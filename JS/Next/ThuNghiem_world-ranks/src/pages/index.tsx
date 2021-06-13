45d074e1a5e0d535ae3def824deec056a9c37ec5
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-06-13 16:27:30 +0900

    feat: get data with getStaticProps

--- JS/Next/ThuNghiem_world-ranks/src/pages/index.tsx
+++ JS/Next/ThuNghiem_world-ranks/src/pages/index.tsx
@@ -1,13 +1,35 @@
+import { InferGetStaticPropsType } from 'next'
 import React from 'react'
 import { Layout } from '../components/layout'
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-const Home = () => {
+import styles from '../styles/home.module.scss'
+
+interface ICountry {
+  name: string
+  region: string
+  subregion: string
+}
+
+const Home = ({
+  countries,
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  console.log({ countries })
   return (
     <Layout>
-      <div className="">hoge</div>
+      <div className={styles.counts}>Found {countries.length} countries</div>
     </Layout>
   )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://restcountries.eu/rest/v2/all')
+  const countries: ICountry[] = await res.json()
+
+  return {
+    props: {
+      countries,
+    },
+  }
 }
 
 export default Home
