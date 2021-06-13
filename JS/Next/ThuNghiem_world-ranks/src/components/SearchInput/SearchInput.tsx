51da2a2d66947613f01331d678a14af2771cc3c7
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-06-13 16:42:12 +0900

    feat: create: SearchInput

---
+++ JS/Next/ThuNghiem_world-ranks/src/components/SearchInput/SearchInput.tsx
@@ -0,0 +1,17 @@
+import { SearchRounded } from '@material-ui/icons'
+import React from 'react'
+
+import styles from './search-input.module.scss'
+
+interface SearchInputProps {}
+
+export const SearchInput = ({}) => {
+  return (
+    <div className={styles.wrapper}>
+      <SearchRounded />
+      <input className={styles.input} />
+    </div>
+  )
+}
+
+
