d3f44a844b6e66cf4ec7a2da9e2bb55bb5bdd703
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-06-17 01:52:05 +0900

    feat: add orderBy func

--- JS/Next/ThuNghiem_world-ranks/src/components/CountriesTable/CountriesTable.tsx
+++ JS/Next/ThuNghiem_world-ranks/src/components/CountriesTable/CountriesTable.tsx
@@ -3,7 +3,18 @@ import { CountryProps, ICountry } from '../../pages'
 
 import styles from './countries-table.module.scss'
 
+const orderBy = (countries: ICountry[], direction: 'asc' | 'desc') => {
+  if (direction === 'asc') {
+    return [...countries].sort((a, b) => (a.population > b.population ? 1 : -1))
+  } else if (direction === 'desc') {
+    return [...countries].sort((a, b) => (a.population > b.population ? -1 : 1))
+  }
+  return countries
+}
+
 export const CountriesTable = ({ countries }: CountryProps) => {
+  const orderCountries = orderBy(countries, 'desc')
+
   return (
     <>
       <div className={styles.heading}>
@@ -11,10 +22,10 @@ export const CountriesTable = ({ countries }: CountryProps) => {
         <button className={styles.heading__population}>Population</button>
       </div>
 
-      {countries.map((c, i) => (
+      {orderCountries.map((c, i) => (
         <div className={styles.row} key={i}>
-          <div className={styles.name}> {c.name} </div>
-          <div className={styles.population}> {c.population} </div>
+          <div className={styles.name}>{c.name}</div>
+          <div className={styles.population}>{c.population}</div>
         </div>
       ))}
     </>
