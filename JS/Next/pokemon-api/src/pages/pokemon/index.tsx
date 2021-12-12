e863176dc0b7de70cf432e0ee2b24e5800134d5b
Yohei Higashi <yoheihigashi.pg@gmail.com>
2021-12-12 13:31:56 +0900

    feat: demo fetch pokemon

---
+++ JS/Next/pokemon-api/src/pages/pokemon/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react'
+import { PokemonList } from '../../components/Pokemon'
+
+const PokemonListPage = () => {
+  return (
+    <>
+      <PokemonList />
+    </>
+  )
+}
+
+export default PokemonListPage
