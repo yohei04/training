c34654fcf729d30e4238c06f8dce21be836cf5d4
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-02-06 12:51:46 +0900

    test: fix create test

--- JS/Next/pokemon-api/src/components/Blog/BlogTemplate/BlogTemplate.test.tsx
+++ JS/Next/pokemon-api/src/components/Blog/BlogTemplate/BlogTemplate.test.tsx
@@ -7,8 +7,8 @@ import { server } from '../../../mocks/server'
 import { BASE_ENDPOINT } from '../../../constant/endpoint'
 import { BlogType } from '../../../types/blog'
 
-describe('ブログリスト', () => {
-  it('データの表示', async () => {
+describe('ブログ', () => {
+  it('データ一覧の表示', async () => {
     render(<BlogTemplate />)
 
     const loading = await screen.findByText('Loading...')
@@ -19,26 +19,26 @@ describe('ブログリスト', () => {
   })
 
   it('データの作成', async () => {
-    const mockFn = jest.fn()
-    server.use(
-      rest.post(`${BASE_ENDPOINT}/blogs`, (req, res, ctx) => {
-        mockFn((req?.body as BlogType).title)
-        return res(ctx.status(200), ctx.delay(500))
-      })
-    )
+    // const mockFn = jest.fn()
+    // server.use(
+    //   rest.post(`/blogs`, (req, res, ctx) => {
+    //     mockFn((req?.body as BlogType).title)
+    //     return res(ctx.status(200), ctx.delay(500))
+    //   })
+    // )
 
     render(<BlogTemplate />)
 
     const input = await screen.findByLabelText('ブログタイトル')
     const button = await screen.findByText('ブログ追加')
-    const text = 'title3'
+    const text = 'title4'
     userEvent.type(input, text)
     userEvent.click(button)
 
-    // const element = await screen.findByText('title3')
-    // expect(element).toBeInTheDocument()
-    await waitFor(() => expect(mockFn).toBeCalledWith(text))
+    const element = await screen.findByText(text)
+    expect(element).toBeInTheDocument()
+    // await waitFor(() => expect(mockFn).toBeCalledWith(text))
 
-    // screen.debug()
+    screen.debug()
   })
 })
