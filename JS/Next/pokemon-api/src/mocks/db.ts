ba86a18694ba55570083fea862fa273ebba30693
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-02-06 22:24:22 +0900

    feat: create user-blog relationships

--- JS/Next/pokemon-api/src/mocks/db.ts
+++ JS/Next/pokemon-api/src/mocks/db.ts
@@ -1,45 +1,57 @@
-import { factory, manyOf, primaryKey } from '@mswjs/data'
+import { factory, manyOf, oneOf, primaryKey } from '@mswjs/data'
 import { BlogType } from '../types/blog'
+import { UserType } from '../types/user'
 
 export const db = factory({
   // A "blog" model that describes what properties
   // each blog has.
+  user: {
+    id: primaryKey(String),
+    name: String,
+    blogs: manyOf('blog'),
+  },
   blog: {
     id: primaryKey(String),
     title: String,
     body: String,
+    // author: oneOf('user'),
   },
 })
+
+export const defaultUsers: UserType[] = [
+  {
+    id: '1',
+    name: 'user1',
+  },
+  {
+    id: '2',
+    name: 'user2',
+  },
+]
 
 export const defaultBlogs: BlogType[] = [
   {
     id: '1',
     title: 'title1',
     body: 'body1',
-    author: 'author1',
+    author: 'user1',
   },
   {
     id: '2',
     title: 'title2',
     body: 'body2',
-    author: 'author2',
+    author: 'user1',
   },
   {
     id: '3',
     title: 'title3',
     body: 'body3',
-    author: 'author3',
+    author: 'user1',
   },
 ]
 
-defaultBlogs.forEach((blog) => db.blog.create(blog))
+// The default data created each time you refresh the page.
+const blogs = defaultBlogs.map((blog) => db.blog.create(blog))
+const users = defaultUsers.map((user) => db.user.create({ ...user, blogs }))
 
-// // The default blogs created each time you refresh the page.
-// db.blog.create({ id: 1, title: 'title1', body: 'body1' })
-// db.blog.create({ id: 2, title: 'Explore the sandbox', body: 'body' })
-// db.blog.create({ id: 3, title: 'Learn about Data', body: 'body' })
-// db.blog.create({
-//   id: 4,
-//   title: 'Install and try it out yourself',
-//   body: 'body',
-// })
+console.log({ users })
--- JS/Next/pokemon-api/src/mocks/handlers.ts
+++ JS/Next/pokemon-api/src/mocks/handlers.ts
@@ -3,6 +3,7 @@ import { db } from './db'
 export const handlers = [
   // Create REST API request handlers based on
   // the "task" database model.
+  ...db.user.toHandlers('rest'),
   ...db.blog.toHandlers('rest'),
 ]
 
