aa798a01a1ceeae955274fbd195d4de80303740c
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-04-17 16:19:34 +0900

    feat: create user

---
+++ JS/Full/nest-crash-course/src/users/users.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
+
+@ApiTags('users')
+@Controller('users')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Get()
+  getUsers(): User[] {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string): User {
+    return this.usersService.findById(Number(id));
+  }
+
+  @Post()
+  createUser(@Body() body: CreateUserDto): User {
+    return this.usersService.createUser(body);
+  }
+}
