0eea8150c1467b94ca76581e391da52426353b85
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-04-23 20:44:12 +0900

    feat: try  startTransition

--- JS/Full/react-suspense-test/src/App.tsx
+++ JS/Full/react-suspense-test/src/App.tsx
@@ -1,14 +1,41 @@
-import { useState } from 'react';
+import { startTransition, Suspense, useState, useTransition } from 'react';
 
+import PhotoList from './componenst/User/PhotoList';
+import UserList from './componenst/User/UserList';
 import UserPage from './componenst/User/UserPage';
 
 function App() {
   const [visible, setVisible] = useState(false);
+  const [radioButtonValue, setRadioButtonValue] = useState('user');
+
+  const isCheckedUser = radioButtonValue === 'user';
+  const isCheckedPhoto = radioButtonValue === 'photo';
+
+  const [isPending, startTransition] = useTransition();
+  const handleRadioButton = (e) => {
+    // startTransition(() => {
+      setRadioButtonValue(e.target.name);
+    // });
+  };
+
+  console.log({ isPending });
 
   return (
     <div>
       <button onClick={() => setVisible((prev) => !prev)}>fetchボタン</button>
       {visible && <UserPage />}
+      <div>
+        <input type="radio" id="user" name="user" onChange={handleRadioButton} checked={isCheckedUser} />
+        <label htmlFor="user">ユーザー</label>
+
+        <input type="radio" id="photo" name="photo" onChange={handleRadioButton} checked={isCheckedPhoto} />
+        <label htmlFor="photo">写真</label>
+      </div>
+
+      <Suspense fallback={<h1 style={{ color: 'tomato' }}>全体をローディング中です........</h1>}>
+        {/* <div style={{ color: isPending ? 'violet' : 'black' }}>{isCheckedUser ? <UserList /> : <PhotoList />}</div> */}
+        {isCheckedUser ? <UserList /> : <PhotoList />}
+      </Suspense>
     </div>
   );
 }
