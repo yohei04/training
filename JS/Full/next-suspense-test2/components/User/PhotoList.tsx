031a2f6dfda30b71ee37aeda16e188d182be2f37
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-04-24 01:27:45 +0900

    feat: add user page

---
+++ JS/Full/next-suspense-test2/components/User/PhotoList.tsx
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import Image from 'next/image';
+import React, { FC } from 'react';
+import { useQuery } from 'react-query';
+
+type Photo = {
+  id: number;
+  title: string;
+  thumbnailUrl: string;
+  url: string;
+};
+
+const PhotoList: FC = () => {
+  const { data, isLoading } = useQuery(['photo'], getPhotos, {
+    suspense: true,
+  });
+
+  console.log({ data });
+
+  return (
+    <ul>
+      {data?.map((d) => (
+        <li key={d.id}>
+          <p>{d.title}</p>
+          <Image src={d.url} alt={d.title} width="100px" height="100px" />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default PhotoList;
+
+const getPhotos = async () => {
+  const data = await axios.get<Photo[]>('https://jsonplaceholder.typicode.com/photos');
+  return data.data.slice(0, 10);
+};
---
+++ JS/Full/next-suspense-test2/components/User/UserList.tsx
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import React, { FC } from 'react';
+import { useQuery } from 'react-query';
+
+type User = {
+  id: number;
+  name: string;
+};
+
+const UserList: FC = () => {
+  const { data, isLoading, isFetching } = useQuery(['user'], getUsers, {
+    suspense: true,
+  });
+
+  return (
+    <ul>
+      {data?.map((d) => (
+        <li key={d.id}>
+          <p>{d.name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default UserList;
+
+const getUsers = async () => {
+  const data = await axios.get<User[]>('https://jsonplaceholder.typicode.com/users');
+  return data.data;
+  // return data.data.slice(0, 5);
+};
