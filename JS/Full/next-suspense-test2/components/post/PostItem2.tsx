950ce78d156b8a5928bc115467d56cadcb1b3162
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-04-27 02:21:05 +0900

    feat: add like

--- JS/Full/next-suspense-test2/components/post/PostItem2.tsx
+++ JS/Full/next-suspense-test2/components/post/PostItem2.tsx
@@ -1,5 +1,8 @@
+import axios from 'axios';
 import { FC, ReactNode } from 'react';
+import { useQuery } from 'react-query';
 
+import { Like } from '../../types/like';
 import { Post } from '../../types/post';
 
 type Props = {
@@ -8,12 +11,23 @@ type Props = {
 };
 
 export const PostItem2: FC<Props> = ({ post, children }) => {
+  // const { data: likes } = useQuery(['likes', post.id], () => getLikes(post.id), {
+  //   suspense: true,
+  //   enabled: !!post.id,
+  // });
+
   return (
     <div style={{ background: 'lightyellow', padding: '1rem', marginBottom: '1rem' }}>
       <p>タイトル：{post.title}</p>
       <p>本文：</p>
       <p>{post.body}</p>
+      {/* <p>{likes?.length}</p> */}
       {children}
     </div>
   );
 };
+
+// const getLikes = async (postId: number) => {
+//   const data = await axios.get<Like[]>(`http://localhost:4000/posts/${postId}/likes`);
+//   return data.data;
+// };
