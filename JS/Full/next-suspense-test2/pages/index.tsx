bf3170584d4b8c530f92454f3715a20cbfea4833
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-04-30 20:43:08 +0900

    fix: 不要なstyleを削除

--- JS/Full/next-suspense-test2/pages/index.tsx
+++ JS/Full/next-suspense-test2/pages/index.tsx
@@ -1,11 +1,9 @@
 import type { NextPage } from 'next';
 import Link from 'next/link';
 
-import styles from '../styles/Home.module.css';
-
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <ul>
         <li>
           <Link href={'/test'}>
