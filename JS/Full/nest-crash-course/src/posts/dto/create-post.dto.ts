8eedadfb893b5b261fa5fee85025f2474ffaa864
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-05-10 02:35:22 +0900

    feat: add validation

--- JS/Full/nest-crash-course/src/posts/dto/create-post.dto.ts
+++ JS/Full/nest-crash-course/src/posts/dto/create-post.dto.ts
@@ -1,6 +1,11 @@
+import { IsNotEmpty, MaxLength, MinLength } from 'class-validator';
+
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePostDto {
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(10)
   @ApiProperty()
   title: string;
 
--- JS/Full/nest-crash-course/src/users/dto/create-user.dto.ts
+++ JS/Full/nest-crash-course/src/users/dto/create-user.dto.ts
@@ -1,6 +1,10 @@
+import { IsNotEmpty, MinLength } from 'class-validator';
+
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @IsNotEmpty()
+  @MinLength(3)
   @ApiProperty()
   name: string;
 }
