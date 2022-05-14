c699059257b428cef32f375d6d681da1a910dee5
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-05-14 22:21:59 +0900

    chore: setting cors

--- JS/Full/nest-crash-course/src/main.ts
+++ JS/Full/nest-crash-course/src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(new ValidationPipe());
 
@@ -18,6 +18,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/', app, document);
 
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
