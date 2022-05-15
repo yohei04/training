033080391dd6d89b6c491fcb12a209fa4565c105
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-05-15 14:03:49 +0900

    feat: create commnet

--- JS/Full/nest-crash-course/src/app.module.ts
+++ JS/Full/nest-crash-course/src/app.module.ts
@@ -5,9 +5,10 @@ import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
 import { PrismaModule } from './prizma/prisma.module';
 import { UsersModule } from './users/users.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [PrismaModule, UsersModule, PostsModule],
+  imports: [PrismaModule, UsersModule, PostsModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
