d0f099e463b1586ad68ad2a94cc33ecceaa8a7b9
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-05-22 17:27:01 +0900

    feat: create ErrorBoundary

--- JS/Full/next-suspense-test2/components/post/PostListSection2.tsx
+++ JS/Full/next-suspense-test2/components/post/PostListSection2.tsx
@@ -7,6 +7,7 @@ import {
   useState,
 } from 'react';
 
+import ErrorBoundary from '../error-boundary/ErrorBoundary';
 import { Spinner } from '../spinner';
 import { CreatePost2, PostList2, PostListSearch2 } from './';
 
@@ -27,9 +28,11 @@ export const PostListSection2: FC<Props> = ({ userId }) => {
       <CreatePost2 userId={userId} />
       <PostListSearch2 searchWord={searchWord} handleSearch={handleSearch} />
       {/* <Suspense fallback={<div className="h-32 bg-yellow-100 "></div>}> */}
-      <Suspense fallback={<Spinner />}>
-        <PostList2 userId={userId} deferredSearchWord={deferredSearchWord} />
-      </Suspense>
+      <ErrorBoundary>
+        <Suspense fallback={<Spinner />}>
+          <PostList2 userId={userId} deferredSearchWord={deferredSearchWord} />
+        </Suspense>
+      </ErrorBoundary>
     </>
   );
 };
