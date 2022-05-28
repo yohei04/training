7042f6622fb2002948261df85c1ef8d110ef838a
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-05-29 00:04:48 +0900

    feat: add validation for comment

--- JS/Full/nest-crash-course/src/comments/dto/create-comment.dto.ts
+++ JS/Full/nest-crash-course/src/comments/dto/create-comment.dto.ts
@@ -1,8 +1,11 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 export class CreateCommentDto {
   @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(10)
   content: string;
 
+  @IsNotEmpty()
   postId: number;
 }
