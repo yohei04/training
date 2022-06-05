10e77c5cd87815ee2e719f57921b22c9e09acbfc
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-06-06 01:19:34 +0900

    chore: add decorator

--- JS/Full/next-suspense-test2/.storybook/preview.js
+++ JS/Full/next-suspense-test2/.storybook/preview.js
@@ -1,9 +1,21 @@
+import { QueryClientProvider, QueryClient } from 'react-query';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+const queryClient = new QueryClient();
+
+export const decorators = [
+  (Story) => (
+    <QueryClientProvider client={queryClient}>
+      <Story />
+    </QueryClientProvider>
+  ),
+];
