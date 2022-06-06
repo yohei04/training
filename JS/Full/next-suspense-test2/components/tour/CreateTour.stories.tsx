306db7795a4f1c4d35ad92199bc3bd8e1a66547d
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-06-06 22:06:19 +0900

    feat: add DuplicatedName story

--- JS/Full/next-suspense-test2/components/tour/CreateTour.stories.tsx
+++ JS/Full/next-suspense-test2/components/tour/CreateTour.stories.tsx
@@ -1,3 +1,5 @@
+import { rest } from 'msw';
+
 import { expect } from '@storybook/jest';
 import {
   ComponentMeta,
@@ -16,19 +18,11 @@ import { CreateTour } from './';
 export default {
   // title: 'Example/Button',
   component: CreateTour,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  // argTypes: {
-  //   backgroundColor: { control: 'color' },
-  // },
 } as ComponentMeta<typeof CreateTour>;
 
 type Story = ComponentStoryObj<typeof CreateTour>;
 
-export const Default: Story = {
-  args: {
-    // primary: true,
-  },
-};
+export const Default: Story = {};
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -39,6 +33,42 @@ export const EmptyName: Story = {
     await sleep(0);
     userEvent.click(button);
     const errorMessage = await canvas.findByText('入力してください');
+    expect(errorMessage).toBeInTheDocument();
+  },
+};
+
+const mockedErrorMessage = '重複していますよおお';
+export const DuplicatedName: Story = {
+  name: '重複した時',
+  parameters: {
+    msw: {
+      handlers: [
+        rest.post('http://localhost:4000/tours', (_, res, ctx) =>
+          res(
+            ctx.status(400),
+            ctx.json({
+              errors: [
+                {
+                  property: 'name',
+                  constraints: {
+                    isUnique: mockedErrorMessage,
+                  },
+                },
+              ],
+            })
+          )
+        ),
+      ],
+    },
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const name = await canvas.getByLabelText('ツアー名：');
+    userEvent.type(name, 'test');
+    const button = await canvas.findByRole('button');
+    await sleep(0);
+    userEvent.click(button);
+    const errorMessage = await canvas.findByText(mockedErrorMessage);
     expect(errorMessage).toBeInTheDocument();
   },
 };
