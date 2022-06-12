f66c96ced45e61136f2fced40e3adc12a7e50ac9
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-06-13 03:18:17 +0900

    chore: add queryClient on storybook

--- JS/Full/next-suspense-test2/.storybook/preview.js
+++ JS/Full/next-suspense-test2/.storybook/preview.js
@@ -1,6 +1,7 @@
-import { QueryClientProvider, QueryClient } from 'react-query';
+import { QueryClientProvider, QueryClient, QueryCache, MutationCache } from 'react-query';
 import { initialize, mswDecorator } from 'msw-storybook-addon';
 import '../styles/globals.css';
+import toast from 'react-hot-toast';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
@@ -12,7 +13,19 @@ export const parameters = {
   },
 };
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error, query) => {
+      toast.error(`Something went wrong: ${error.message}`);
+    },
+  }),
+  mutationCache: new MutationCache({
+    onError: (error) => {
+      toast.error(`入力に誤りがあります`);
+    },
+  }),
+});
+
 // Initialize MSW
 initialize();
 
