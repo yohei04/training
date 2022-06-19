7443bf869f7e7db9e7cda8b7e8f0fa8a3fa7463a
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-06-19 23:14:15 +0900

    feat: modify TodoItem

--- JS/Full/next-suspense-test2/components/todo/list/TodoItem/TodoItem.tsx
+++ JS/Full/next-suspense-test2/components/todo/list/TodoItem/TodoItem.tsx
@@ -1,15 +1,25 @@
 import { FC, memo } from 'react';
 
 import { Todo } from '../';
+import style from './TodoItem.module.css';
 
 type Props = {
   todo: Todo;
+  toggleTodo: (id: number) => void;
 };
 
-export const TodoItem: FC<Props> = memo(({ todo }) => {
+export const TodoItem: FC<Props> = memo(({ todo, toggleTodo }) => {
   return (
     <li>
-      <div>{todo.name}</div>
+      <div className={style.root}>
+        <input
+          id={`todo-${todo.id}`}
+          type="checkbox"
+          defaultChecked={todo.completed}
+          onChange={() => toggleTodo(todo.id)}
+        />
+        <label htmlFor={`todo-${todo.id}`}>{todo.name}</label>
+      </div>
     </li>
   );
 });
--- JS/Full/next-suspense-test2/components/todo/list/TodoListTemplate.tsx
+++ JS/Full/next-suspense-test2/components/todo/list/TodoListTemplate.tsx
@@ -1,6 +1,6 @@
-import { FC, useState } from 'react';
+import { FC, useCallback, useState } from 'react';
 
-import { CreateTodo, TodoList } from './';
+import { CreateTodo, TodoItem, TodoList } from './';
 
 const initTodos = [
   {
@@ -11,6 +11,26 @@ const initTodos = [
   {
     id: 2,
     name: 'Todo 2',
+    completed: true,
+  },
+  {
+    id: 3,
+    name: 'Todo 3',
+    completed: false,
+  },
+  {
+    id: 4,
+    name: 'Todo 4',
+    completed: false,
+  },
+  {
+    id: 5,
+    name: 'Todo5',
+    completed: true,
+  },
+  {
+    id: 6,
+    name: 'Todo 6',
     completed: false,
   },
 ];
@@ -24,14 +44,25 @@ export type Todo = {
 export const TodoListTemplate: FC = () => {
   const [todos, setTodos] = useState<Todo[]>(initTodos);
 
-  const addTodo = (name: string) => {
-    setTodos([...todos, { id: (todos.at(-1)?.id ?? 0) + 1, name, completed: false }]);
-  };
+  const addTodo = useCallback((name: string) => {
+    setTodos((prev) => [...prev, { id: (prev.at(-1)?.id ?? 0) + 1, name, completed: false }]);
+  }, []);
+
+  const toggleTodo = useCallback((id: number) => {
+    setTodos((prev) =>
+      prev.map((todo) => (todo.id === id ? { ...todo, completed: !todo.completed } : todo))
+    );
+  }, []);
 
   return (
     <section className="p-5">
       <CreateTodo addTodo={addTodo} />
-      <TodoList todos={todos} />
+      {/* <TodoList todos={todos} /> */}
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} />
+        ))}
+      </ul>
     </section>
   );
 };
