762d66f617ca8846e4a9375b9ec9ae2b40f68fd5
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-10-03 23:24:03 +0900

    feat: add controller

--- Ktolin/demo/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ Ktolin/demo/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,10 +2,24 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class DemoApplication
 
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
+}
+
+data class Message(val id: String?, val text: String)
+
+@RestController
+class MessageResource {
+	@GetMapping
+	fun index(): List<Message> = listOf(
+		Message("1", "Hello!"),
+		Message("2", "Bonjour!"),
+		Message("3", "Privet!"),
+	)
 }
