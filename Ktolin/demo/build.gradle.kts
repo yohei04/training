98dee657cf5111e49009b9dbe7963bba6bb82f9e
Yohei Higashi <yoheihigashi.pg@gmail.com>
2022-10-04 02:11:52 +0900

    feat: add database support

--- Ktolin/demo/build.gradle.kts
+++ Ktolin/demo/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.14.RELEASE"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
-	kotlin("plugin.jpa") version "1.6.21"
 }
 
 group = "com.example"
@@ -17,7 +16,7 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
--- Ktolin/demo/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ Ktolin/demo/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -4,6 +4,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.data.annotation.Id
+import org.springframework.data.jdbc.repository.query.Query
+import org.springframework.data.relational.core.mapping.Table
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 
 @SpringBootApplication
 class DemoApplication
@@ -12,14 +19,32 @@ fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
 }
 
-data class Message(val id: String?, val text: String)
+@Table("MESSAGES")
+data class Message(@Id val id: String?, val text: String)
 
 @RestController
-class MessageResource {
+class MessageResource(val service: MessageService) {
 	@GetMapping
-	fun index(): List<Message> = listOf(
-		Message("1", "Hello!"),
-		Message("2", "Bonjour!"),
-		Message("3", "Privet!"),
-	)
+	fun index(): List<Message> = service.findMessages()
+
+	@PostMapping
+	fun post(@RequestBody message: Message) {
+		service.post(message)
+	}
+}
+
+
+interface MessageRepository : CrudRepository<Message, String> {
+	@Query("select * from messages")
+	fun findMessages(): List<Message>
+}
+
+@Service
+class MessageService(val db: MessageRepository) {
+
+	fun findMessages(): List<Message> = db.findMessages()
+
+	fun post(message: Message){
+		db.save(message)
+	}
 }
